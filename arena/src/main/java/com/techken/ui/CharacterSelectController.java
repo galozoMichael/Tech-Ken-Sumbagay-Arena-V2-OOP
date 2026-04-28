ac13ee08b6bac469b0c109d048638cacee461013
karlVincentIllusorio <karlvincent.illusorio@cit.edu>
2026-04-28 21:13:09 +0800

    feat(ui): add back button navigation in Character Select to MainMenu

--- arena/src/main/java/com/techken/ui/CharacterSelectController.java
+++ arena/src/main/java/com/techken/ui/CharacterSelectController.java
@@ -2,10 +2,30 @@ package com.techken.ui;
 
 import com.techken.MainApp;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class CharacterSelectController {
+    @FXML private Button backBtn;
+
     @FXML
     public void initialize() {
         MainApp.playMusic("menutestaudio.mp3");
+    }
+
+    @FXML
+    public void backToMenu() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenu.fxml"));
+            Scene scene = new Scene(loader.load(), 1280, 720);
+            scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+            Stage stage = (Stage) backBtn.getScene().getWindow();
+            stage.setScene(scene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
